8ab240cf9ae87538261837dd85316477c5c7fe14
totalnomad <alexcahiz@gmail.com>
2017-04-09 15:02:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const _ = require('lodash');
 const Script = require('smooch-bot').Script;
+
+const scriptRules = require('./script.json');
 
 module.exports = new Script({
     processing: {
-        prompt: (bot) => bot.say('Beep boop...'),
+        //prompt: (bot) => bot.say('Beep boop...'),
         receive: () => 'processing'
     },
 
